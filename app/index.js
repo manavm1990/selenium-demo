df177e354a6abe0f921bd1058f81402c09d3051a
Manav Misra <manavm@visionwebsoft.com>
2022-07-22 17:32:43 -0500

    Run tests from interactive menu

--- app/index.js
+++ app/index.js
@@ -5,16 +5,27 @@ const choice = await inquirer.prompt([
     type: "list",
     name: "action",
     message: "Which test would you like to run?",
-    choices: ["1. Google Search with Selenium and JavaScript"],
+    choices: [
+      "1. Google Search with Selenium and JavaScript",
+      "2. Yahoo Sign In Link with Username and Checkbox",
+    ],
   },
 ]);
 
 switch (parseInt(choice.action, 10)) {
   case 1: {
-    console.log("Running Google Search with Selenium and JavaScript");
+    console.info("🏃🏾‍♂️ Google Search with Selenium and JavaScript");
 
     const googleTest = await import("./test-google-search.js");
     googleTest.default();
+    break;
+  }
+
+  case 2: {
+    console.info("🏃🏾‍♂️ Yahoo Sign In Link with Username and Checkbox");
+
+    const yahooTest = await import("./test-yahoo-login.js");
+    yahooTest.default();
     break;
   }
 
--- app/test-yahoo-login.js
+++ app/test-yahoo-login.js
@@ -1,19 +1,21 @@
 import { Builder, By } from "selenium-webdriver";
 
-const driver = await new Builder().forBrowser("chrome").build();
+export default async () => {
+  const driver = await new Builder().forBrowser("chrome").build();
 
-// Open the Yahoo homepage
-await driver.get("https://www.yahoo.com/");
+  // Open the Yahoo homepage
+  await driver.get("https://www.yahoo.com/");
 
-// Click the 'Sign In' link
-await driver.findElement(By.linkText("Sign in")).click();
+  // Click the 'Sign In' link
+  await driver.findElement(By.linkText("Sign in")).click();
 
-// Type in the username
-await driver.findElement(By.name("username")).sendKeys("tester");
+  // Type in the username
+  await driver.findElement(By.name("username")).sendKeys("tester");
 
-// If the 'Stay signed in' checkbox is checked, uncheck it
-if (await driver.findElement(By.name("persistent")).isSelected()) {
-  await driver.findElement(By.css("label[for='persistent']")).click();
-}
+  // If the 'Stay signed in' checkbox is checked, uncheck it
+  if (await driver.findElement(By.name("persistent")).isSelected()) {
+    await driver.findElement(By.css("label[for='persistent']")).click();
+  }
 
-driver.quit();
+  driver.quit();
+};
